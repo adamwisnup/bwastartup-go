3ca4b0bacc59297ddbd3883790e5203a3d5f8489
adamwisnup <wisnuadam21@gmail.com>
2024-08-02 04:28:52 +0700

    feat: add Login feature

--- handler/user.go
+++ handler/user.go
@@ -8,20 +8,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type UserHandler struct {
+type userHandler struct {
 	userService user.Service
 }
 
-func NewUserHandler(userService user.Service) *UserHandler {
-	return &UserHandler{userService}
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
 }
 
-func (h *UserHandler) RegisterUser(c *gin.Context){
+func (h *userHandler) RegisterUser(c *gin.Context){
 	var input user.RegisterUserInput
 
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
-		errors := helper.FormatValidationError(err)
+		errors := helper.FormatValidationError(err)	
 		errorMessage := gin.H{"errors": errors}
 
 		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
@@ -40,4 +40,33 @@ func (h *UserHandler) RegisterUser(c *gin.Context){
 
 	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", user)
 	c.JSON(http.StatusOK, response)
+}
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedinUser, "token123")
+
+	response := helper.APIResponse("Successfuly loggedin", http.StatusOK, "success", formatter)
+	c.JSON(http.StatusOK, response)
+
 }
